ff556c8f804d95cbd7c179d3ef776a647a357c43
marcos.gonzalez <marcos.gonzalez@imatia.com>
2022-10-18 12:29:04 +0200

    App with CRUD operations, Material design and Routes protection

---
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+
+@Component({
+  selector: 'app-buscar',
+  templateUrl: './buscar.component.html',
+  styleUrls: ['./buscar.component.css']
+})
+export class BuscarComponent implements OnInit {
+
+  termino: string  = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado: Heroe | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
